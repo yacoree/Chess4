1521b4307a6eeccf1a6ddf5e4cd45164b22f8997
Ильяс Юсупов <iu70980@gmail.com>
2022-06-18 12:43:51 +0300

    созадл шахматное поле

--- ChessWPF/MainWindow.xaml.cs
+++ ChessWPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ChessWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
